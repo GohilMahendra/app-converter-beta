f249953777689b44b301054ee03ba65ceedadfc5
Mahendra Gohil <mgohil984@rku.ac.in>
2021-03-23 13:06:33 +0530

    new upload

---
+++ App.js
@@ -0,0 +1,122 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,Dimensions,
+  Image,
+  View,
+} from 'react-native';
+
+import {  NavigationContainer} from "@react-navigation/native";
+import {createStackNavigator  } from "@react-navigation/stack";
+import HomeScreen from './screens/HomeScreen';
+
+const mystack=createStackNavigator()
+const App=()=>
+{
+
+  const height=Dimensions.get('screen').height
+  const width=Dimensions.get('screen').width
+  const horizontalAnimation = {
+    gestureDirection: 'horizontal',
+    cardStyleInterpolator: ({ current, layouts }) => {
+      return {
+        cardStyle: {
+          transform: [
+            {
+              translateX: current.progress.interpolate({
+                inputRange: [0, 1],
+                outputRange: [layouts.screen.width, 0],
+              }),
+            },
+          ],
+        },
+      };
+    },
+  };
+
+  function splash({navigation}) {
+    //
+
+   
+ //   const test=require()   
+ setTimeout(()=>{navigation.replace("Home"),5000})
+     return(
+
+     
+       <View style={{flex:1,justifyContent:'center',
+       alignItems:'center'}}>
+
+      <Image
+      style={{resizeMode:'cover',height:height,width:width}}
+      source={require("./build_data/splash.jpg")}
+      
+      >
+
+      </Image>
+      
+             </View>
+     )
+     
+   }
+
+  return(
+
+    
+    <NavigationContainer
+
+    >
+    <mystack.Navigator
+    initialRouteName="splash"
+    
+    >
+
+    <mystack.Screen
+   
+   name="splash"
+   component={splash}
+    options=
+    {
+      {
+        headerShown:false
+
+      }
+    }
+    >
+
+    </mystack.Screen>
+    <mystack.Screen
+   
+   name="Home"
+   component={HomeScreen}
+    options=
+    {
+      {
+        headerShown:false
+
+      }
+    }
+    >
+
+    </mystack.Screen>
+
+    </mystack.Navigator>
+
+      </NavigationContainer>
+
+    
+    )
+}
+
+
+
+export default App;
---
+++ screens/HomeScreen.js
@@ -0,0 +1,166 @@
+
+
+import React,{useRef,useState} from "react";
+
+import {View,Text, ScrollView,Dimensions,Image,BackHandler,RefreshControl,
+    ActivityIndicator
+
+} from "react-native";
+import WebView from "react-native-webview";
+import  data  from "../build_data/data.json";
+
+console.log(data)
+const HomeScreen=()=>
+{
+
+      const [refr,setrefr]=useState(false)
+  const [canGoBack, setCanGoBack] = useState(false)
+  const [canGoForward, setCanGoForward] = useState(false)
+  const [currentUrl, setCurrentUrl] = useState('')
+  
+
+  let jsCode = `
+        var cookie={};
+        document.cookie.split('; ').forEach(function(i){cookie[i.split('=')[0]]=i.split('=')[1]});
+        document.querySelector('#email').value=cookie['email'] || '';
+        document.querySelector('#password').value=cookie['password'] || '';
+        document.querySelector('#login button').onclick = function(){
+            document.cookie = 'email='+document.querySelector('#email').value;
+            document.cookie = 'password='+document.querySelector('#password').value;
+        };
+    `;
+  const height=Dimensions.get('window').height
+  const width=Dimensions.get('window').width
+    const [load,setload]=React.useState(false)
+    const webviewRef = useRef()
+  
+    const errorpage=()=>
+    {
+      return(
+        <View style={{flex:1}}>
+          <Image source={require('../build_data/splash.jpg')}
+          style={{height:height,width:width}}
+          ></Image>
+           </View>
+      )
+    }
+   const backButtonHandler = () => {
+    if (webviewRef.current) webviewRef.current.goBack()
+    }
+  
+    const refc=()=>
+  {
+    if (webviewRef.current)  webviewRef.current.reload()
+  
+  
+    setrefr(true)
+  }
+  const  handleBackButton = ()=>{
+    if (webviewRef.current) webviewRef.current.goBack()
+      return true;
+    }
+  const frontButtonHandler = () => {
+      if (webviewRef.current) webviewRef.current.goForward()
+    }
+  
+  
+  
+    React.useEffect
+    {
+      
+      BackHandler.addEventListener('hardwareBackPress',handleBackButton)
+      ,[]
+    }
+   
+  React.useEffect(
+          ()=>{internetPermission},
+      )
+      const internetPermission = async () => {
+          try {
+            const granted = await PermissionsAndroid.request(
+              PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+              {
+                title: "Cool Photo App Camera Permission",
+                message:
+                  "Cool Photo App needs access to your camera " +
+                  "so you can take awesome pictures.",
+                buttonNeutral: "Ask Me Later",
+                buttonNegative: "Cancel",
+                buttonPositive: "OK"
+              }
+            );
+            if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+              console.log("You can use the camera");
+            } else {
+              internetPermission
+              console.log("Camera permission denied");
+            }
+          } catch (err) {
+            console.warn(err);
+          }
+        };
+  
+
+    return(   
+        <View style={{flex:1}}>    
+
+<ScrollView 
+
+
+refreshControl={<RefreshControl refreshing={refr}
+onRefresh={refc}
+></RefreshControl>}
+style={{height:height,width:width,position:'absolute'}}>
+
+  
+             <WebView
+
+source={{uri:data.url}}
+ref={webviewRef}
+style={{flex:1,height:height,width:width}}
+  //  onLoadProgress={()=>setload(true)}
+    onLoadEnd={()=>{setload(false),setrefr(false),console.log('success')}}
+    
+
+    pullToRefreshEnabled={true}
+    allowFileAccessFromFileURLs={true}
+    allowingReadAccessToURL={true}
+    allowsBackForwardNavigationGestures={true}
+    allowsInlineMediaPlayback={true}
+
+    
+    allowUniversalAccessFromFileURLs={true}
+    userAgent="Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"
+
+    message={console.log()}
+    onNavigationStateChange={navState => {
+      setCanGoBack(navState.canGoBack)
+      setCanGoForward(navState.canGoForward)
+      setCurrentUrl(navState.url)}}
+    
+
+      onMessage={(event)=> console.log(event.nativeEvent.data)}
+      injectedJavaScript={jsCode}
+    oninte
+   // onNavigationStateChange={}
+   onLoadStart={()=>setload(true)}
+     onHttpError={()=>errorpage()}
+   onError={()=>errorpage()}
+     javaScriptEnabled={true}
+     cacheEnabled={true}
+     sharedCookiesEnabled={true}
+     thirdPartyCookiesEnabled={true}
+     >
+
+
+     </WebView>
+  {load &&   <ActivityIndicator style={{backgroundColor:'white',
+  height:50,width:50,borderRadius:50,position:"absolute",
+  top:height/2,alignSelf:'center'}}
+   size='large' color="gray" animating={true} ></ActivityIndicator>}
+</ScrollView>
+</View>
+    )
+}
+
+export default HomeScreen
