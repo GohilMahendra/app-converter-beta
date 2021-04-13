869d047657ebcbc6c092b03872eb7a0df1bbd810
Mahendra Gohil <mgohil984@rku.ac.in>
2021-04-13 19:59:09 +0530

    add

--- App.js
+++ App.js
@@ -58,7 +58,7 @@ const App=()=>
 
       <Image
       style={{resizeMode:'cover',height:height,width:width}}
-      source={require("../splash.jpg")}
+      source={require("./splash.jpg")}
       
       >
 
